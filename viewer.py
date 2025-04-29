50343fdf68085881cd1ac93fb717a8b3a0b24817
Diana Castilleja <diana.castilleja01@utrgv.edu>
2025-04-29 17:07:03 -0500

    my changes from dianabranch

--- viewer.py
+++ viewer.py
@@ -2,17 +2,21 @@ import mujoco
 from mujoco.viewer import launch
 
 # Load the MuJoCo model
-# model = mujoco.MjModel.from_xml_path("myo_sim/arm/myoarm.xml")
-
 # model = mujoco.MjModel.from_xml_path("myo_sim/torso/myotorso_rigid.xml")
-
 # model = mujoco.MjModel.from_xml_path("myo_sim/leg/myolegs_abdomen.xml")
-
 # model = mujoco.MjModel.from_xml_path("myo_sim/arm/leftarm.xml") 
-# 
-# model = mujoco.MjModel.from_xml_path("myo_sim/arm/leftarmtest.xml") 
+#model = mujoco.MjModel.from_xml_path("arm/leftarm.xml")
+ 
 
-model = mujoco.MjModel.from_xml_path("myo_sim/arm/lefthand.xml") 
+
+
+
+#model = mujoco.MjModel.from_xml_path("arm/myoarm.xml")
+model = mujoco.MjModel.from_xml_path("arm/myoarm.xml")
+#model = mujoco.MjModel.from_xml_path("arm/assets/myoarm_body.xml")
+
+
+#model = mujoco.MjModel.from_xml_path("arm/leftarmtest.xml") 
 
 data = mujoco.MjData(model)
 
